82dc04b8a4b299ddb6a4493ac2d90fba7e6407cf
v.karmazin <v.karmazin@tinkoff.ru>
2024-05-15 17:21:41 +0200

    feat: support avi in splits script

--- scripts/split_videos.sh
+++ scripts/split_videos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Specify the input/output directory containing your mp4 files
+# Specify the input/output directory containing your videos
 input_dir="data/MSRVTT/videos/all"
 output_dir="data/MSRVTT/videos/5sec"
 num_jobs=12  # processors
@@ -14,10 +14,18 @@ split_video() {
     local file="$1"
     local output_dir="$2"
     local clip_duration="$3"
-    local filename=$(basename "$file" .mp4)
-    ffmpeg -i "$file" -acodec copy -f segment -segment_time $clip_duration -vcodec copy -reset_timestamps 1 -map 0 "$output_dir/$filename-%04d.mp4"
+    local filename=$(basename "$file")
+    local extension="${filename##*.}"
+
+    if [[ $extension == "mp4" ]]; then
+        ffmpeg -i "$file" -acodec copy -f segment -segment_time $clip_duration -vcodec copy -reset_timestamps 1 -map 0 "$output_dir/${filename%.*}-%04d.mp4"
+    elif [[ $extension == "avi" ]]; then
+        ffmpeg -i "$file" -acodec copy -bsf:v h264_mp4toannexb -f segment -segment_time $clip_duration -vcodec copy -reset_timestamps 1 -map 0 "$output_dir/${filename%.*}-%04d.avi"
+    else
+        echo "Unsupported format for file: $file"
+    fi
 }
 export -f split_video
 
-# Run split_video function in parallel for each mp4 file
-find "$input_dir" -type f -name '*.mp4' | parallel split_video {} "$output_dir" "$clip_duration"
+# Run split_video function in parallel for each video file
+find "$input_dir" -type f \( -name '*.mp4' -o -name '*.avi' \) | parallel split_video {} "$output_dir" "$clip_duration"
