81efe4ce3e1dedd304426aed23a2c031379309d4
v.karmazin <v.karmazin@tinkoff.ru>
2024-06-26 17:33:33 +0200

    style: pretty

--- src/custom_components/script.js
+++ src/custom_components/script.js
@@ -20,13 +20,13 @@ Array.from(buttons).forEach(function(button) {
 });
 
 close.onclick = function() {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 };
 
 window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
-    }
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
 };
 
 // ----------------------------------------------------
@@ -47,97 +47,97 @@ const COMPONENT_READY = "streamlit:componentReady"
 const SET_FRAME_HEIGHT = "streamlit:setFrameHeight"
 
 function _sendMessage(type, data) {
-// copy data into object
-var outboundData = Object.assign({
-  isStreamlitMessage: true,
-  type: type,
-}, data)
+  // copy data into object
+  var outboundData = Object.assign({
+    isStreamlitMessage: true,
+    type: type,
+  }, data)
 
-if (type == SET_COMPONENT_VALUE) {
-  console.log("_sendMessage data: " + JSON.stringify(data))
-  console.log("_sendMessage outboundData: " + JSON.stringify(outboundData))
-}
+  if (type == SET_COMPONENT_VALUE) {
+    console.log("_sendMessage data: " + JSON.stringify(data))
+    console.log("_sendMessage outboundData: " + JSON.stringify(outboundData))
+  }
 
-window.parent.postMessage(outboundData, "*")
+  window.parent.postMessage(outboundData, "*")
 }
 
 function initialize(pipeline) {
 
-// Hook Streamlit's message events into a simple dispatcher of pipeline handlers
-window.addEventListener("message", (event) => {
-  if (event.data.type == RENDER) {
-    // The event.data.args dict holds any JSON-serializable value
-    // sent from the Streamlit client. It is already deserialized.
-    pipeline.forEach(handler => {
-      handler(event.data.args)
-    })
-  }
-})
+  // Hook Streamlit's message events into a simple dispatcher of pipeline handlers
+  window.addEventListener("message", (event) => {
+    if (event.data.type == RENDER) {
+      // The event.data.args dict holds any JSON-serializable value
+      // sent from the Streamlit client. It is already deserialized.
+      pipeline.forEach(handler => {
+        handler(event.data.args)
+      })
+    }
+  })
 
-_sendMessage(COMPONENT_READY, { apiVersion: 1 });
+  _sendMessage(COMPONENT_READY, { apiVersion: 1 });
 
-// Component should be mounted by Streamlit in an iframe, so try to autoset the iframe height.
-window.addEventListener("load", () => {
-  window.setTimeout(function () {
-    setFrameHeight(document.documentElement.clientHeight)
-  }, 0)
-})
+  // Component should be mounted by Streamlit in an iframe, so try to autoset the iframe height.
+  window.addEventListener("load", () => {
+    window.setTimeout(function () {
+      setFrameHeight(document.documentElement.clientHeight)
+    }, 0)
+  })
 
-// Optionally, if auto-height computation fails, you can manually set it
-// (uncomment below)
-//setFrameHeight(200)
+  // Optionally, if auto-height computation fails, you can manually set it
+  // (uncomment below)
+  //setFrameHeight(200)
 }
 
 function setFrameHeight(height) {
-_sendMessage(SET_FRAME_HEIGHT, { height: height })
+  _sendMessage(SET_FRAME_HEIGHT, { height: height })
 }
 
 // The `data` argument can be any JSON-serializable value.
 function notifyHost(data) {
-_sendMessage(SET_COMPONENT_VALUE, data)
+  _sendMessage(SET_COMPONENT_VALUE, data)
 }
 
 // ----------------------------------------------------
 // Your custom functionality for the component goes here:
 
 function toggle(button, group) {
-buttons = document.getElementsByClassName(group)
-console.log(buttons)
-for (let i = 0; i < buttons.length; i++) {
-  console.log(buttons.item(i))
-}
-
-for (let i = 0; i < buttons.length; i++) {
-  if (button.id == String(i) & button.className.includes("off")) {
-    button.className = group + " on"
-  } else if (button.id == String(i) & button.className.includes("on")) {
-    button.className = group + " off"
-  } else {
-    buttons.item(i).className = group + " off"
+  buttons = document.getElementsByClassName(group)
+  console.log(buttons)
+  for (let i = 0; i < buttons.length; i++) {
+    console.log(buttons.item(i))
   }
-}
 
-buttons = document.getElementsByClassName(group)
-actions = []
-for (let i = 0; i < buttons.length; i++) {
-  btn = buttons.item(i)
-  actions.push({ "action": btn.value, "value": btn.className.includes("on") })
-}
-
-states = {}
-states['choice'] = {
-  "name": group,
-  "state": {
-    "action": button.value,
-    "value": button.className.includes("on")
+  for (let i = 0; i < buttons.length; i++) {
+    if (button.id == String(i) & button.className.includes("off")) {
+      button.className = group + " on"
+    } else if (button.id == String(i) & button.className.includes("on")) {
+      button.className = group + " off"
+    } else {
+      buttons.item(i).className = group + " off"
+    }
   }
-}
-states["options"] = { "name": group, "states": actions }
 
-notifyHost({
-  value: states,
-  dataType: "json",
-})
+  buttons = document.getElementsByClassName(group)
+  actions = []
+  for (let i = 0; i < buttons.length; i++) {
+    btn = buttons.item(i)
+    actions.push({ "action": btn.value, "value": btn.className.includes("on") })
+  }
+
+  states = {}
+  states['choice'] = {
+    "name": group,
+    "state": {
+      "action": button.value,
+      "value": button.className.includes("on")
+    }
+  }
+  states["options"] = { "name": group, "states": actions }
+
+  notifyHost({
+    value: states,
+    dataType: "json",
+  })
 }
 
 // ----------------------------------------------------
@@ -146,17 +146,17 @@ notifyHost({
 
 // Set initial value sent from Streamlit!
 function initializeProps_Handler(props) {
-let header1 = document.getElementById("group_1_header")
+  let header1 = document.getElementById("group_1_header")
 
-let header2 = document.getElementById("group_2_header")
+  let header2 = document.getElementById("group_2_header")
 }
 // Access values sent from Streamlit!
 function dataUpdate_Handler(props) {
-let msgLabel = document.getElementById("message_label")
+  let msgLabel = document.getElementById("message_label")
 }
 // Simply log received data dictionary
 function log_Handler(props) {
-console.log("Received from Streamlit: " + JSON.stringify(props))
+  console.log("Received from Streamlit: " + JSON.stringify(props))
 }
 
 let pipeline = [initializeProps_Handler, dataUpdate_Handler, log_Handler]
@@ -165,11 +165,3 @@ let pipeline = [initializeProps_Handler, dataUpdate_Handler, log_Handler]
 // Finally, initialize component passing in pipeline
 
 initialize(pipeline)
-
-
-
-
-
-window.addEventListener('scroll', () => {
-  console.log(`X: ${window.scrollX}, Y: ${window.scrollY}`);
-});
