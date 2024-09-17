563ab3cf688a194a7ef0a1bdcc65fe4ff91d979a
v.karmazin <v.karmazin@tinkoff.ru>
2024-09-17 12:54:54 +0200

    feat: added modalities to collection info response

--- backend/src/handler/info.py
+++ backend/src/handler/info.py
@@ -4,11 +4,14 @@ from pydantic import BaseModel, RootModel
 from pymilvus import Collection
 from pymilvus.orm import utility
 
+from src.entity.embedder import Modality
+
 
 class IndexInfo(BaseModel):
     dataset: str
     version: str
     count: int
+    modalities: list[Modality]
 
 
 IndexesInfoResponse = RootModel[list[IndexInfo]]
@@ -26,6 +29,9 @@ class InfoHandler(IInfoHandler):
         infos = []
         for collection_name in collections:
             collection = Collection(collection_name)
+            modalities = [partition.name for partition in collection.partitions if partition.name != "_default"]
             dataset, version = collection_name.split("__")
-            infos.append(IndexInfo(dataset=dataset, version=version, count=collection.num_entities))
+            infos.append(
+                IndexInfo(dataset=dataset, version=version, count=collection.num_entities, modalities=modalities)
+            )
         return IndexesInfoResponse(infos)
